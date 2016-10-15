e505721ef0d6e27e6a2b4842076b334a0247e8d1
Daniel Morgan <me@danielmorgan.co.uk>
2016-10-15 18:46:22 +0100

    Pointless tinkering

--- src/Game.ts
+++ src/Game.ts
@@ -1,15 +1,27 @@
 export class Game {
     private game: Phaser.Game;
-    private text: Phaser.Text;
+    private emitter: any;
 
     public constructor(selector: string = 'game') {
-        this.game = new Phaser.Game(800, 600,
+        const resolution = { width: window.innerWidth,
+                            height: window.innerHeight,
+                            ratio: window.innerWidth / window.innerHeight };
+
+        this.game = new Phaser.Game('100%',
+                                    '100%',
                                     Phaser.AUTO,
                                     selector,
                                     { preload: this.preload,
                                       create: this.create,
                                       update: this.update,
                                       render: this.render });
+    }
+
+    public fire(pointer) {
+        console.log('firing', pointer);
+        const firework = this.game.add.sprite(pointer.worldX, pointer.worldY, 'firework');
+        this.game.physics.arcade.enable(firework);
+        firework.body.collideWorldBounds = true;
     }
 
     private preload() {
@@ -19,22 +31,18 @@ export class Game {
     private create() {
         console.log('create');
 
-        this.text = this.game.add.text(this.game.width / 2,
-            this.game.height / 2,
-            'Hello World', 
-            { font: '32px Arial',
-              fill: '#ffffff',
-              align: 'center' });
-
-        this.text.anchor = new Phaser.Point(0.5, 0.5);
+        this.game.world.setBounds(0, 0, this.game.width, this.game.height);
+        this.game.physics.arcade.gravity.y = -100;
+        this.game.input.onTap.add(this.fire.bind(this), this);
     }
 
     private update() {
         console.log('update');
 
-        this.text.rotation += 0.03;
+        this.emitter.renderer.clear(0.05);
     }
 
     private render() {
+      this.game.debug.cameraInfo(this.game.camera, 32, 32);
     }
 }
